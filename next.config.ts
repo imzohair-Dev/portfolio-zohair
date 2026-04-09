4522f3e07bafb258c63ff1872ab05ff1d5c751ba
imzohair-Dev <imzohair9@gmail.com>
2026-04-09 20:06:42 +0500

    fix deployment

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: '/portfolio-zohair',
   output: 'export',
+  basePath: '/portfolio-zohair', 
   images: {
     unoptimized: true,
   },
