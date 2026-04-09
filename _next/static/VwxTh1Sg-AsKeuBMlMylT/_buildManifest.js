9dc3499b108992812f57eb4da463da9607e4cf52
imzohair-Dev <imzohair-Dev@users.noreply.github.com>
2026-04-09 16:16:10 +0000

    deploy: 9c556105512f7b8b746d3db857fad2f1f1da0eda

--- _next/static/VwxTh1Sg-AsKeuBMlMylT/_buildManifest.js
+++ _next/static/VwxTh1Sg-AsKeuBMlMylT/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/portfolio-zohair//_next/:path+",
+        "destination": "/portfolio-zohair/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
